7d16bc18bc3719d4e9ce8e0f3c32510efe1f826e
Edgar Cobos <edgarcobos00@gmail.com>
2019-09-16 22:25:26 -0300

    Edit file.rkt file
    Add test submodule with tests
    Fix the recursive definition of my-*

--- labs/L03/file.rkt
+++ labs/L03/file.rkt
@@ -6,9 +6,16 @@
       (my-+ (sub1 a) (add1 b))))
 
 (define (my-* a b)
-  (if (zero? a)
-      b
-      (my-* (sub1 a) (my-+ b b))))
+  (cond
+    [(zero? a) 0]
+    [(= 1 a) b]
+    [else (my-+ (my-* (sub1 a) b) b)]))
 
 (provide my-+
          my-*)
+
+(module+ test
+  (require rackunit)
+  (check-equal? (my-+ 1 1) 2 "Simple addition")
+  (check-equal? (my-* 1 2) 2 "Simple multiplication")
+  (check-equal? (my-* 3 2) 6 "Simple multiplication"))
