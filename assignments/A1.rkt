0486612e264c227385a61af2723ca4e06d0e0d6f
Edgar Cobos <edgarcobos00@gmail.com>
2019-09-19 23:07:18 -0300

    Add Q2 to A1.rkt
    - include test submodule for Q2

--- assignments/A1.rkt
+++ assignments/A1.rkt
@@ -11,3 +11,11 @@
 (module+ test
   (require rackunit)
   (check-equal? (drop-divisible 3 (list 2 3 4 5 6 7 8 9 10)) (list 2 3 4 5 7 8 10)))
+
+(define (sieve-with divlist list)
+  (cond
+    ((empty? divlist) list)
+    (else (drop-divisible (first divlist) (sieve-with (rest divlist) list)))))
+
+(module+ test
+  (check-equal? (sieve-with '(2 3) (list 2 3 4 5 6 7 8 9 10)) (list 2 3 5 7)))
