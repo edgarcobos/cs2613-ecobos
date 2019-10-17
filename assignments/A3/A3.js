bf6504ea9a61715ccd3af586da378e035ff51983
Edgar Cobos <edgarcobos00@gmail.com>
2019-10-17 19:50:08 -0300

    Add Q2 to A3

--- assignments/A3/A3.js
+++ assignments/A3/A3.js
@@ -15,4 +15,27 @@ function read_json_file(filename) {
 
 exports.read_json_file=read_json_file;
 
+//applies flatten to json file with mailing list and returns a list of mails with the specified properties
+function summarize_mail(filename) {
+    let data = flatten(read_json_file(filename));
+    let lst = [];
+    if(arguments.length < 2) {
+        for(let i in data) {
+            lst[i] = {};
+        }
+        return lst;
+    }
+    let curr;
+    for(let j in data) {
+        curr = {};
+        for(var k = 1; k < arguments.length; k++) {
+            curr[arguments[k]] = data[j].headers[arguments[k]];
+        }
+        lst[j] = curr;
+    }
+    return lst;
+}
+
+exports.summarize_mail = summarize_mail;
+
 
