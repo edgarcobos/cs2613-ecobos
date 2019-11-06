91d1df13f5becfdbeda0a144e4c894f13beb1379
Edgar Cobos <edgarcobos00@gmail.com>
2019-11-06 15:42:34 -0400

    Add select function from A4
    include tests

--- assignments/A4/A4.py
+++ assignments/A4/A4.py
@@ -14,3 +14,12 @@ def read_csv(filename):
 def header_map(row):
     """Build a dictionary from labels to column numbers"""
     return {row[i]: i for i in range(len(row))}
+    
+def select(table, dct):
+    """Create a new table with specific columns of the given table""" 
+    lst = []
+    header = header_map(table[0])
+    vals = [header[key] for key in header if key in dct]
+    for row in table:
+        lst.append([row[i] for i in vals])
+    return lst
--- assignments/A4/test_A4_1.py
+++ assignments/A4/test_A4_1.py
@@ -1,4 +1,4 @@
-from A4 import read_csv, header_map
+from A4 import read_csv, header_map, select
 
 def test_read_csv():
     assert read_csv('test1.csv') == [['name', 'age', 'eye colour'],
@@ -11,3 +11,16 @@ table = read_csv('test1.csv')
 def test_header_map_1():
     hmap = header_map(table[0])
     assert hmap == { 'name': 0, 'age': 1, 'eye colour': 2 }
+
+def test_select_1():
+    assert select(table,{'name','eye colour'}) == [['name', 'eye colour'],
+                                                   ['Bob',  'blue'],
+                                                   ['Mary', 'brown'],
+                                                   ['Vij',  'green']]
+
+def test_select_2():
+    """Select with unordered dictionary"""
+    assert select(table,{'eye colour','age'}) == [['age', 'eye colour'],
+                                                   ['5',  'blue'],
+                                                   ['27', 'brown'],
+                                                   ['54',  'green']]
