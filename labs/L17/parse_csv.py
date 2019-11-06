09899a9822c0f2bf4e829da813a1da074bfb0f6e
Edgar Cobos <edgarcobos00@gmail.com>
2019-11-06 11:18:14 -0400

    Add part 3 of the lab

--- labs/L17/parse_csv.py
+++ labs/L17/parse_csv.py
@@ -7,7 +7,7 @@ def split_csv(string):
         #rows.append([strip_quotes(col) for col in row.split(",")])
     #return rows
     return [split_row(row) for row in string.splitlines()]
-"""
+
 def strip_quotes(string):
         strip_regex = re.compile(r'^"?([^"]+)"?$')
         search = strip_regex.search(string)
@@ -15,7 +15,7 @@ def strip_quotes(string):
             return search.group(1)
         else:
             return None
-
+"""
 def split_row_3(string):
     split_regex=re.compile(
         r'''^   # start
