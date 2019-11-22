9bf649a383fecff57c06cab44c2e62a23525d64c
Edgar Cobos <edgarcobos00@gmail.com>
2019-11-22 00:00:15 -0400

    Add Token class of A5
    include tests

--- assignments/A5/A5.py
+++ assignments/A5/A5.py
@@ -8,3 +8,26 @@ class Type(Enum):
     IDENT = 3
     OPEN = 4
     TRANSFER = 5
+
+class Token:
+    def __init__(self, type, value):
+        '''initializes the values of type and value for new objects'''
+        self.type = type
+        self.value = value
+
+    def __str__(self):
+        '''returns objects, currency is "pretty printed" as dollars and cents'''
+        if self.type.name == 'CURRENCY':
+            return f'[{self.type.name}: {int(self.value)//100}.{int(self.value)%100}]'
+        else:
+            return f'[{self.type.name}: {self.value}]'
+
+    def __eq__(self, other):
+        '''checks if two token objects have the same attributes, case insensitive for our language keywords'''
+        if isinstance(self, other.__class__):
+            if self.type.name == 'IDENT' or self.type.name == 'CURRENCY':
+                return vars(self) == vars(other)
+            else:
+                return self.type == other.type and self.value.lower() == other.value.lower()
+        else:
+            return NotImplemented
--- assignments/A5/test_A5.py
+++ assignments/A5/test_A5.py
@@ -1,6 +1,34 @@
-from A5 import Type
+from A5 import Type, Token
 
 def test_enum():
     '''check that defined enum type matches assignment'''
 
     assert sorted([ member.name for member in Type ]) == ["BALANCE", "CURRENCY", "IDENT", "OPEN", "TRANSFER"]
+
+def test_token():
+    token=Token(Type.IDENT,"hello")
+    assert token.type==Type.IDENT
+    assert token.value=="hello"
+
+def test_str():
+    id=Token(Type.IDENT,"hello")
+    assert str(id) == '[IDENT: hello]'
+    money=Token(Type.CURRENCY,10042)
+    assert str(money) == '[CURRENCY: 100.42]'
+
+def test_equal_ident():
+    assert Token(Type.IDENT,"Bob") == Token(Type.IDENT,"Bob")
+    assert Token(Type.IDENT,"Bob") != Token(Type.IDENT,"bOb")
+
+def test_equal_keywords():
+    assert Token(Type.TRANSFER,"transfer") != Token(Type.OPEN,"open")
+    assert Token(Type.OPEN,"OPEN") == Token(Type.OPEN,"open")
+    assert Token(Type.BALANCE,"BALANCE") == Token(Type.BALANCE,"balance")
+
+def test_equal_currency():
+    assert Token(Type.CURRENCY,1000) == Token(Type.CURRENCY,1000)
+    assert Token(Type.CURRENCY,1000) != Token(Type.CURRENCY,1001)
+
+def test_equal_bad():
+    '''test for class mismatch'''
+    assert Token(Type.CURRENCY,1000) != '[Type.CURRENCY, 10.00]'
